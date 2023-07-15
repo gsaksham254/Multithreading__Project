a5c9264657d8477ae75d8dc8d36522d88dc00335
saksham <gsaksham254@gmail.com>
2023-07-15 13:38:31 +0530

    Checkup The File

---
+++ Data.java
@@ -0,0 +1,18 @@
+public class Data {
+    class Consumer extends Thread{
+        Company c;
+        Consumer(Company c)
+        {
+            this.c=c;
+        }
+    
+        public void run()
+        {
+            while(true)
+            {
+                this.c.consume_item();
+                try{Thread.sleep(2000);}catch(Exception e){}
+            }
+        }
+    }
+}
